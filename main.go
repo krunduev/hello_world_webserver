25b7affdc49e6a48fa059f11b64112636b292278
Konstantin Runduev <runduev@gmail.com>
2022-05-29 15:23:06 +0300

    Initial commit

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = fmt.Fprintf(w, "ok")
-		fmt.Printf("ping, %v\n", time.Now().String())
+		fmt.Printf("%v, %v\n", r.RequestURI, time.Now().Format("2006-01-02 15:04:05"))
 	})
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
